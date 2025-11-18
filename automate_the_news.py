dc3afdfb2680a6bba595f94ce6d4a67dfa6d4609
Charlene <charlene.flores.404@gmail.com>
2025-11-18 22:25:43 +0800

    Automate the news: find elements

--- automate_the_news.py
+++ automate_the_news.py
@@ -3,5 +3,19 @@ from selenium import webdriver
 web = 'https://www.thesun.co.uk/sport/football/'
 
 # Creating the driver
+
 driver = webdriver.Chrome()
 driver.get(web)
+
+# Finding Elements
+
+# explore the browser Inspect tab and find the right elements
+# use xpath; can also use classname, id, or other attributes
+containers = driver.find_elements(by='xpath', value='//div[@class="story__copy-container"]')
+
+# driver.find_element(by='xpath', value='//div[@class="story__copy-container"]/a/p')
+for container in containers:
+    title = container.find_element(by='xpath', value='./a/p').text
+    subtitle = container.find_element(by='xpath', value='./a/h3').text
+
+    container.find_element(by='xpath', value='./a').get_attribute('href')
