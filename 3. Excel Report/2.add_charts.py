c9cc964defb94f2d9b9f8977c8fa9037f6b4dfab
Charlene <charlene.flores.404@gmail.com>
2025-11-19 19:37:55 +0800

    Automate excel report: add a bar chart

---
+++ 3. Excel Report/2.add_charts.py
@@ -0,0 +1,48 @@
+from openpyxl import load_workbook
+from openpyxl.chart import BarChart, Reference
+import os
+
+# Get the directory where the script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Paths for input and output files
+input_path = os.path.join(script_dir,'pivot_table.xlsx')
+output_path = os.path.join(script_dir, 'barchart.xlsx')
+
+# Read workbook and select sheet
+wb = load_workbook(input_path)
+sheet = wb['Report']
+
+# Active rows and columns
+min_column = wb.active.min_column
+max_column = wb.active.max_column
+min_row = wb.active.min_row
+max_row = wb.active.max_row
+
+# Instantiate a barchart
+barchart = BarChart()
+
+# Locate data and categories
+data = Reference(sheet,
+                 min_col=min_column+1,
+                 max_col=max_column,
+                 min_row=min_row,
+                 max_row=max_row)  # including headers
+
+categories = Reference(sheet,
+                       min_col=min_column,
+                       max_col=min_column,
+                       min_row=min_row+1,
+                       max_row=max_row)  # not including headers
+
+# Adding data and categories
+barchart.add_data(data, titles_from_data=True)
+barchart.set_categories(categories)
+
+# Make chart
+sheet.add_chart(barchart, "B12")
+barchart.title = 'Sales by Product line'
+barchart.style = 2  # choose the chart style
+
+# Save workbook
+wb.save(output_path)
